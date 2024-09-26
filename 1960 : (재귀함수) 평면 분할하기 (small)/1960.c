9cb9700fd9134b164966251acf82ba2f8e3087fd
dohun08 <bemik624@gmail.com>
2024-09-26 20:49:47 +0900

    진필쌤 숙제

--- 1960 : (재귀함수) 평면 분할하기 (small)/1960.c
+++ 1960 : (재귀함수) 평면 분할하기 (small)/1960.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
+int memo[100000];
 int f(int n){
-    if(n==0){
-        return 1;
-    }
-    else{
-        return (f(n-1) + n)%137;
-    }
+    if(n==0) return 1;
+    else return memo[n] = (f(n-1) + n)%137;
 }
 int main(){
     int n;
