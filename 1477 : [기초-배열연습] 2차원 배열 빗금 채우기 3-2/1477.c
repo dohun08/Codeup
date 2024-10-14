5ab32db88286d8da37aabe49b19fe776d3608da2
dohun08 <bemik624@gmail.com>
2024-10-14 22:33:54 +0900

    1477 c

---
+++ 1477 : [기초-배열연습] 2차원 배열 빗금 채우기 3-2/1477.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int arr[201][201]={0};
+int main(){
+	int i, j, k, n, m, num=1;
+	scanf("%d %d",&n, &m);
+	k=2;//k= i+j의 값
+  
+	//배열의 인덱스 위치에 값을 저장 코드 작성
+	for(k=2; k<=n+m; k++){
+		for(i=1; i<=n; i++){
+			for(j=1; j<=m; j++){
+				if(i+j==k){
+					arr[i][j] = num++;
+				}
+			}
+		}
+	}
+	
+	// 배열의 결과를 출력
+	for(i=1; i<=n; i++, puts("")){
+		for(j=1; j<=m; j++){
+			printf("%d ", arr[i][j]);
+		}
+	}
+	return 0;
+}
