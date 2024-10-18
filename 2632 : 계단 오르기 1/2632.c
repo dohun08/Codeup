78b355e5a000d717e2c0d857a92ac4b8e1c0e0c3
dohun08 <bemik624@gmail.com>
2024-10-18 23:03:11 +0900

    2632 c

---
+++ 2632 : 계단 오르기 1/2632.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int dp[100001]={1, 1, 2};
+int f(int n){
+    if(n==1) return 1;
+
+    if(dp[n]!=0) return dp[n];
+    
+	else return dp[n] = (f(n-1) + f(n-2));
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("%d", f(n));
+    return 0;
+}
+
