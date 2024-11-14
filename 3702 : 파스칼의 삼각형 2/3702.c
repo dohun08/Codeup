26c0f1f8d585c1ac979691dfcc7743f509706d31
dohun08 <bemik624@gmail.com>
2024-11-14 23:45:54 +0900

    3702 c refactor

--- 3702 : 파스칼의 삼각형 2/3702.c
+++ 3702 : 파스칼의 삼각형 2/3702.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int memo[1001][1001];
 int f(int n, int r){
-    if(n==1 || r==1) return memo[n][r] = 1;
-    else if(memo[n][r]) return memo[n][r]%100000000;
-    else return memo[n][r] = f(n-1, r)%100000000 + f(n, r-1)%100000000;
+    if(memo[r][n]) return memo[r][n];
+    else if(n == 1 || r == 1) return memo[n][r]=1;
+    else return memo[n][r] = memo[r][n] = (f(n-1, r) + f(n, r-1))%100000000;
 }
 
 int main(){
--- 3702 : 파스칼의 삼각형 2/tempCodeRunnerFile.c
+++ 3702 : 파스칼의 삼각형 2/tempCodeRunnerFile.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
-#define MAX 51
-#define MOD 100000000
-long long int d[MAX][MAX] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,}};
-long long int num, i=1, j, k;
-int f(int n, int m){
-    if(n==j && i==m){
-        printf("%lld", d[i-1][j-1]%MOD);
-        return 0;
-    }
-    else if(j==0){
-        d[i][j]=1;
-        j++;
-        num++;
-        f(n, m);
-    }
-    else if(50==j){
-        i++;
-        j=0;
-        num = 0;
-        f(n, m);
-    }
-    else{
-        d[i][j] = d[i-1][j] + d[i][j-1];
-        j++;
-        num++;
-        f(n, m);
-    }
+int memo[1001][1001];
+int f(int n, int r){
+    if(memo[r][n]) return memo[r][n];
+    else if(n == 1 || r == 1) return memo[n][r]=1;
+    else return memo[n][r] = memo[r][n] = f(n-1, r) + f(n, r-1);
 }
+
 int main(){
-    int n, m;
-    scanf("%d %d", &n, &m);
-    f(n, m);
+    int n, r;
+    scanf("%d %d", &n, &r);
+    printf("%d", f(n, r)%100000000);
     return 0;
 }
